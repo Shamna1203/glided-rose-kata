b3d8078d0b8281ad129e6fa013010c22b7ecb15f
shamna <shamnakhan@gmail.com>
2026-05-23 02:23:31 +0200

    GREEN: Aged Brie quality increases by 2 after sellIn

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -30,4 +30,10 @@ it("Aged Brie: quality increases by 1 each day", function() {
     expect(items[0].quality).toEqual(21);
 });
 
+it("Aged Brie: quality increases by 2 after sellIn date passed", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(22);
+});
+
 });
